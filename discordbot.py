6d660115d0eba5a2492d5d04c7968fc4048d316c
Will <xiaodown@gmail.com>
2024-12-14 16:04:10 -0800

    updating to not respond to self when he says his own name

--- discordbot.py
+++ discordbot.py
@@ -1,5 +1,5 @@
 import discord
-import chat
+import openai_bot_interface
 import asyncio
 from datetime import datetime, timedelta
 import random
@@ -21,7 +21,7 @@ last_channel_id = 0
 async def check_inactivity():
     # In theory, if the bot has spoken, but hasn't spoken for 2-3 hours, he will
     # say something in the last channel he spoke in.
-    # God only knows if this will work, async shit is janky.
+    # God only knows if this will break, globals with async seems like inviting disaster.
     deltaminutes = (120 + random.randint(1, 59))
     await bot.wait_until_ready()
     global last_message_time
@@ -30,7 +30,7 @@ async def check_inactivity():
         if bot_has_ever_spoken:
             if datetime.now() - last_message_time > timedelta(minutes=deltaminutes):
                 print(f"Two hours and {deltaminutes - 120} has passed without a message.")
-                message = chat.get_ai_idle_musing()
+                message = openai_bot_interface.get_ai_idle_musing()
                 guild = bot.get_guild(last_guild_id)
                 if guild:
                     channel = guild.get_channel(last_channel_id)
@@ -56,16 +56,18 @@ async def on_ready():
 async def on_message(message):
     # Last message time updated here to cover all channels the bot is in.
     # This means he'll only speak if the server is truely dead.
+    # If he's on multiple servers, they'd all have to be dead - this is really 
+    # meant to only be on one server for small group amusement.
     global last_message_time
     last_message_time = datetime.now()
     print(f"Message from {message.author}: {message.content}")
-    if bot_name.lower() in message.content.lower():
+    if bot_name.lower() in message.content.lower() and message.author != bot.user:
         global bot_has_ever_spoken
         bot_has_ever_spoken = True
         global last_guild_id
         last_guild_id = message.guild.id
         global last_channel_id
         last_channel_id = message.channel.id
-        await message.channel.send(chat.get_ai_response(message.content))
+        await message.channel.send(openai_bot_interface.get_ai_response(message.content))
 
 bot.run(api_key)
