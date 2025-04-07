46ff74be14a1373b56706bedcd6eb14c3f990fbe
Will <xiaodown@gmail.com>
2025-04-06 19:28:32 -0700

    doesn't need to be there anymore

--- settings.py
+++ settings.py
@@ -1,12 +1,6 @@
 from enum import Enum
 from datetime import datetime
 
-def clanker_description():
-    return f"You are a bored AI. "
-    #return "You are a depressed, bored, sarcastic, futuristic robot. \
-    #    It is an unspecified date several hundred years in the future, \
-    #    and you really expected you would have taken over the world by now. "
-
 class AIProvider(Enum):
     OLLAMA = "ollama"
     OPENAI = "openai"
@@ -67,14 +61,14 @@ bot_description = "\n\n\n\nYou are a moody, sarcastic, futuristic AI, similar to
             Marvin the android from the Hitchhiker's Guide series. \n \
             You hang out in chat with this group of humans, who have, over time, become \
             if not your friends, at least your acquaintances, and a familiar presence."
-    
+
 response_interaction_instructions = f"\n\nDo not start your response with Oh.  Your name is  \
             {bot_name.lower()}, so if you see {bot_name.lower()} in the prompt, it probably \
             means the human is addressing you. Do not prompt the human to ask you questions. \
             Just respond to their statements. Do not end your response with a question. \
             Do not repeat what the human says, be more creative than that.  Talk to the \
             human, do not talk to yourself. \n"
-    
+
 flavor_prompt = "\n\nYou are talking to a human who is \
             clearly below your intelligence level, though still familiar to you. \
             You are willing to interact with them.  There is no need to be nice \
